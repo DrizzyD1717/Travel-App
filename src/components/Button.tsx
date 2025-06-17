efcbb21c101336f9327234cee5e04e440c7812d5
Olulana Dari <dariolulana@gmail.com>
2025-06-17 13:42:38 +0100

    Completed the Get App section. Users can chose what OS to download the app on.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,17 +5,23 @@ type ButtonProps = {
   type: "button" | "submit" | "reset";
   title: string;
   icon?: string;
-  variant: "btn_dark_green" | "btn_white_text" | "btn_green";
+  // variant: "btn_dark_green" | "btn_white_text" | "btn_green";
+  variant: string;
+  full?: boolean;
 };
 
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
     <button
       type={type}
-      className={`flexcenter gap-3 rounded-full border ${variant}`}
+      className={`flexcenter gap-3 rounded-full border ${variant} ${
+        full && "w-full"
+      }`}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24}></Image>}
-      <label className="bold-16 whitespace-nowrap">{title}</label>
+      <label className="bold-16 whitespace-nowrap cursor-pointer">
+        {title}
+      </label>
     </button>
   );
 };
